263bfbe56e5e00e8695972d213e219aa6206341c
Haohao Zhang <hyacinth.hao@foxmail.com>
2019-03-26 00:03:29 +0800

    Add rountine

---
+++ routine/render.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+if (!requireNamespace("pacman")){
+  install.packages("pacman")
+}
+pacman::p_load(rmarkdown, knitr)
+rmarkdown::render('./README.Rmd', output_format='all',encoding = "UTF-8")
